9b666d656dfe21fd26ae9bef52b61823f2201e39
yudax42 <yudax42@gmail.com>
2024-06-12 09:06:14 +0100

    fix: change embedding model to match the index

--- packages/worker/src/config/constants.ts
+++ packages/worker/src/config/constants.ts
@@ -3,4 +3,4 @@ import { TaskStatus } from '~/common/interfaces/train.interface'
 export const ALLOWED_TRAINING_SOURCES = ['url', 'sitemap', 'pdf'] as const
 export const TASK_STATUS = Object.values(TaskStatus)
 
-export const EMBEDDINGS_MODEL = '@cf/baai/bge-base-en-v1.5'
+export const EMBEDDINGS_MODEL = '@cf/baai/bge-small-en-v1.5'
--- packages/worker/src/lib/utils/ai/store.ts
+++ packages/worker/src/lib/utils/ai/store.ts
@@ -7,7 +7,7 @@ import { EMBEDDINGS_MODEL } from '~/config/constants'
 
 export default function (env: Bindings) {
   switch (EMBEDDINGS_MODEL) {
-    case '@cf/baai/bge-base-en-v1.5': {
+    case '@cf/baai/bge-small-en-v1.5': {
       const embeddings = new CloudflareWorkersAIEmbeddings({
         binding: env.AI as unknown as Fetcher,
         model: EMBEDDINGS_MODEL,
