943a21fe3d8c14db91dad0f2a30b278d91299d95
yudax42 <yudax42@gmail.com>
2024-06-16 17:09:37 +0100

    feat: cors allow * origin

--- packages/worker/src/api/http/app.ts
+++ packages/worker/src/api/http/app.ts
@@ -1,41 +1,41 @@
-import type { Context } from 'hono'
-import { Hono } from 'hono'
-import { ZodError } from 'zod'
-import { defaultRoutes } from './routes'
-import { ServiceError } from '~/lib/utils/service-error'
-import ResponseHandler from '~/lib/utils/response-handler'
+import type { Context } from "hono";
+import { Hono } from "hono";
+import { ZodError } from "zod";
+import { defaultRoutes } from "./routes";
+import { ServiceError } from "~/lib/utils/service-error";
+import ResponseHandler from "~/lib/utils/response-handler";
+import { cors } from "hono/cors";
 
-import type {
-  Environment,
-} from '~/common/interfaces/common.interface'
+import type { Environment } from "~/common/interfaces/common.interface";
 
-const app = new Hono<Environment>()
+const app = new Hono<Environment>();
 
-app.options('*', (c) => {
-  return ResponseHandler.success(c, 'ok')
-})
+app.use("*", cors());
+
+app.options("*", (c) => {
+	return ResponseHandler.success(c, "ok");
+});
 
 app.notFound((c) => {
-  throw ServiceError.notFound(`404: ${c.req.url}`)
-})
+	throw ServiceError.notFound(`404: ${c.req.url}`);
+});
 
 app.onError((err: any, c) => {
-  console.error(err)
-  if (err instanceof ZodError)
-    return ResponseHandler.validationErr(c, err)
-  else if (err instanceof ServiceError)
-    return ResponseHandler.genericError(c, err.message, err.statusCode)
-  else return ResponseHandler.serverError(c, 'internal server error')
-})
+	console.error(err);
+	if (err instanceof ZodError) return ResponseHandler.validationErr(c, err);
+	else if (err instanceof ServiceError)
+		return ResponseHandler.genericError(c, err.message, err.statusCode);
+	else return ResponseHandler.serverError(c, "internal server error");
+});
 
 defaultRoutes.forEach((route: any) => {
-  app.route(`${route.path}`, route.route)
-})
+	app.route(`${route.path}`, route.route);
+});
 
-app.get('/whoami', (c: Context) => {
-  return ResponseHandler.success(c, {
-    name: 'Supervisor service',
-  })
-})
+app.get("/whoami", (c: Context) => {
+	return ResponseHandler.success(c, {
+		name: "Supervisor service",
+	});
+});
 
-export default app
+export default app;
