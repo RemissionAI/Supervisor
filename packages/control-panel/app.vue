e444295099e14cdbb0fd9246b767671cdd074b76
yudax42 <yudax42@gmail.com>
2024-06-16 17:54:31 +0100

    feat: basic prototype

--- packages/control-panel/app.vue
+++ packages/control-panel/app.vue
@@ -1,34 +1,14 @@
-<template>
-  <UContainer>
-
-    <UCard>
-      <template #header>
-        <h1 class="text-3xl my-3 font-bold">Lupus AI Supervisor</h1>
-        <p class="text-base mb-10">I'm an AI trained on dataset</p>
-      </template>
-
-      <UInput icon="i-heroicons-sparkles-20-solid" @keyup.enter="askQuestion" size="xl" color="white" :trailing="false"
-        placeholder="Ask a question" focus />
-        
-      <pre v-if="response">{{ response }}</pre>
-
-      <template #footer>
-        <Placeholder class="h-8" />
-      </template>
-    </UCard>
-
-
-
-  </UContainer>
-</template>
-
-<script setup>
-import { ref } from 'vue';
+<script lang="ts" setup>
+import { ref, nextTick } from 'vue';
 
 const query = ref('');
 const response = ref('');
+const isLoading = ref(false);
+const inputRef = ref(null);
 
 const askQuestion = async () => {
+  isLoading.value = true;
+
   try {
     const res = await fetch('https://remission-supervisor-service-staging.remissionaiorg.workers.dev/supervisor/v1/ask', {
       method: 'POST',
@@ -42,11 +22,38 @@ const askQuestion = async () => {
     const data = await res.json();
     response.value = data.data.kwargs.content;
   } catch (error) {
-    console.error('Error:', error);
     response.value = 'Error fetching response';
+  } finally {
+    query.value = '';
+    isLoading.value = false;
+    nextTick(() => {
+      inputRef.value.focus();
+    });
   }
 };
 </script>
+
+<template>
+  <UContainer>
+    <UCard>
+      <template #header>
+        <h1 class="text-3xl my-3 font-bold">Lupus AI Supervisor</h1>
+        <p class="text-base mb-10">I'm an AI trained on dataset</p>
+      </template>
+
+      <UInput ref="inputRef" icon="i-heroicons-sparkles-20-solid" @keyup.enter="askQuestion" size="xl"
+        :color="isLoading ? 'green' : 'white'" :trailing="false" placeholder="Ask a question" focus v-model="query"
+        :disabled="isLoading" />
+
+      <div class="max-w-full text-base mt-12" v-show="response">
+        <MarkdownRenderer :source="response"/>
+      </div>
+
+      <template #footer></template>
+    </UCard>
+  </UContainer>
+</template>
+
 
 <style scoped>
 .container {
---
+++ packages/control-panel/components/MarkdownRenderer.vue
@@ -0,0 +1,120 @@
+<script setup lang="ts">
+import MarkdownIt from 'markdown-it';
+
+const markdown = new MarkdownIt()
+
+defineProps({
+    source: {
+        type: String,
+        default: ""
+    }
+});
+</script>
+
+<template>
+    <div v-html="markdown.render(source)" class="markdown-content" />
+</template>
+
+<style lang="scss" scoped>
+.markdown-content {
+    max-width: 100%;
+    overflow-x: auto;
+
+    h1,
+    h2,
+    h3,
+    h4 {
+        margin-top: 1em;
+        margin-bottom: 0.5em;
+        font-weight: 600;
+        line-height: 1.2;
+    }
+
+    h1 {
+        font-size: 2em;
+    }
+
+    h2 {
+        font-size: 1.5em;
+    }
+
+    h3 {
+        font-size: 1.17em;
+    }
+
+    h4 {
+        font-size: 1em;
+    }
+
+    p {
+        margin-top: 0;
+        margin-bottom: 1em;
+    }
+
+    ul,
+    ol {
+        margin-top: 0;
+        margin-bottom: 2em;
+    }
+
+    li {
+        margin-top: 0.25em;
+    }
+
+    img {
+        max-width: 100%;
+        height: auto;
+        margin-top: 2em;
+    }
+
+    pre,
+    code {
+        font-family: monospace;
+    }
+
+    pre {
+        padding: 1em;
+        background-color: #f5f5f5;
+        border-radius: 5px;
+        overflow: auto;
+    }
+
+    code {
+        padding: 0.2em 0.4em;
+        background-color: #f1f1f1;
+        border-radius: 3px;
+    }
+
+    blockquote {
+        padding: 0 1em;
+        color: #6a737d;
+        border-left: 0.25em solid #dfe2e5;
+        margin: 0;
+    }
+
+    table {
+        width: 100%;
+        border-collapse: collapse;
+    }
+
+    th,
+    td {
+        padding: 0.5em;
+        border: 1px solid #ddd;
+    }
+
+    th {
+        font-weight: bold;
+        background-color: #f8f8f8;
+    }
+
+    a {
+        color: #0366d6;
+        text-decoration: none;
+
+        &:hover {
+            text-decoration: underline;
+        }
+    }
+}
+</style>
