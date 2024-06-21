f40caf271ae68f5b712eb73c4ae40e2d4b59fb02
Youssef <yudax42@gmail.com>
2024-06-21 11:29:59 +0100

    feat: add links filters to sitemap (#24)

--- packages/worker/src/api/http/controllers/train.controller.ts
+++ packages/worker/src/api/http/controllers/train.controller.ts
@@ -2,6 +2,7 @@ import type { Context } from 'hono'
 import * as TrainService from '~/core/services/train.service'
 import ResponseHandler from '~/lib/utils/response-handler'
 import { TrainingTaskRepository } from '~/core/repositories/train.repository'
+import DataLoader from '~/lib/utils/ai/data-loader'
 
 export async function load(c: Context) {
   const body = await c.req.json()
@@ -21,6 +22,12 @@ export async function loadFile(c: Context) {
   return ResponseHandler.success(c, {
     message: 'Noice',
   })
+}
+
+export async function loadSitemap(c: Context) {
+  const data = (await DataLoader.sitemap('https://www.lupus.org/sitemap.xml')).map(link => link.loc)
+
+  return ResponseHandler.success(c, data)
 }
 
 export async function list(c: Context) {
